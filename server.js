919f0400903467ec0f908b8747b9c8d8d22faaf6
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-07-19 18:49:52 -0700

    added file routes to the get and post requests

--- server.js
+++ server.js
@@ -3,32 +3,34 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+//link the get request to the notes html document
 app.get('/notes', (req, res) => {
-console.log("lets bang this out!")
+    res.sendFile(path.join(__dirname, './miniature-eureka-main/develop/public/notes.html'));
 });
 
-
-
+//api notes get and post requests
 app.get('/api/notes', (req, res) => {
     res.send('hello there')
+    res.json(allNotes.slice(1));
 });
-
-
 
 app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    //tell the server what to do with json data returned 
+    res.json(newNote);
 });
 
-
-
+//delete notes 
 app.delete('/api/notes/:id', (req, res) => {
-
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
 });
 
 
 
 app.get('*', (req, res) => {
     res.send('hi');
+    res.sendFile(path.join(__dirname, './miniature-eureka-main/develop/public/index.html'))
 });
 
 
